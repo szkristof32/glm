1f137839288fcb3065ef0ef6162b25328e7f541c
szkristof32 <kristofreg@gmail.com>
2023-10-01 15:35:47 +0200

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -12,3 +12,26 @@ project "glm"
 		"glm/**.hpp",
 		"glm/**.inl"
 	}
+	
+	includedirs
+	{
+		"glm"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		systemversion "latest"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
