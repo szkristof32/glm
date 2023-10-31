57e8fc6100154096838303a3c3f1ee00c8305c80
szkristof32 <kristofreg@gmail.com>
2023-10-31 14:47:10 +0100

    Updated .gitignore and premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,8 @@ project "glm"
 	language "C++"
 	cppdialect "C++20"
 	
-	targetdir ("%{wks.location}/bin")
-	objdir ("%{wks.location}/bin-int")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 	
 	files
 	{
